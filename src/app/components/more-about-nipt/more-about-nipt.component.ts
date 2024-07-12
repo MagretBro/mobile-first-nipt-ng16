f725e7a5012d6762370547968eabd1bc1ff03372
MagretBro <magretbro@gmail.com>
2024-07-12 18:09:34 +0700

    поправлена верстка header / more about nipt

--- src/app/components/more-about-nipt/more-about-nipt.component.ts
+++ src/app/components/more-about-nipt/more-about-nipt.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./more-about-nipt.component.scss']
 })
 export class MoreAboutNiptComponent {
+  activeBtn: string = 'WHAT is NIPT';
 
+  setActiveBtn(button: string) {
+    this.activeBtn = button;
+  }
+
+  isActive(button: string): boolean {
+    return this.activeBtn === button;
+  }
 }
